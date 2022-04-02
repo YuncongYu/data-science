9e1dec614901ff70e96f2a7dcada531ddc0c5227
Yuncong Yu <yuncong.yu@outlook.com>
2022-04-02 13:39:58 +0200

    Completed uber pickups tutorial

--- streamlit/uber/uber_pickups.py
+++ streamlit/uber/uber_pickups.py
@@ -2,4 +2,48 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# Title
 st.title("Uber pickups in NYC")
+
+
+DATE_COLUMN = 'date/time'
+DATA_URL = 'https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz'
+
+
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    # lowercase = lambda x: str(x).lower()
+    data.rename(str.lower, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+
+# Loading data
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text('Done! (using st.cache)')
+
+# Data frame
+if st.checkbox("Show raw data"):
+    st.subheader("Raw data")
+    st.write(data)
+
+# Histogram
+st.subheader("Number of pickups by hour")
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
+st.bar_chart(hist_values)
+
+# # Map
+# The data frame must have columns called 'lat', 'lon', 'latitude', or 'longitude'.
+# st.subheader("Map of all pickups")
+# st.map(data)
+
+# Map with filter
+hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f"Map of all pickups at {hour_to_filter}:00")
+st.map(filtered_data)
